d461d40c98a4cb95ad195e09ca725b8d8422f956
Roshila-code <twanabasuroshila@gmail.com>
2026-06-02 16:29:32 +0900

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom";
+
 function Products({ list }) {
   return (
     <div>
       <h2>Product List</h2>
+
       <div className="product-list">
-      {
-        list.map((product) => (
+        {list.map((product) => (
           <div key={product.id} className="product-item">
             <h3>{product.name}</h3>
 
-            <img src="/routertest/image/laptop.png"alt={product.name} width={150} />
+            <img
+              src={product.image}
+              alt={product.name}
+              width={150}
+            />
 
             <p>{product.price}</p>
-            <Link to={`/productlist/${product.id}`}>View Detail</Link>
-        
+
+            <Link to={`/productlist/${product.id}`}>
+              View Detail
+            </Link>
           </div>
-        ))
-      }
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default Products;
