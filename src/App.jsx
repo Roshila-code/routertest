359e39c9fb76a7fdd96a2cb94dbf25365556c8ab
Roshila-code <twanabasuroshila@gmail.com>
2026-06-02 11:27:42 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route, Link, NavLink}
+from 'react-router-dom'
+import Contact from './components/Contact';
+import Products from './components/Product';
+import ProductDetail from './components/ProductDetail';
+import "./NavLinkTest.css";
+
+const products = [
+  {id : 1, name:"Laptop",price:1200, image:"/image/laptop.png"},
+   { id : 2, name:"Keyboard",price:100, image:"/image/keyboard.png"},
+    {id : 3, name:"Mouse",price:200, image:"/image/mouse.png"}
+  
+]
+
+const Home = () => <h2>Welcome to my Home</h2>
+
+function About() {
+  return (
+    <h2>This is a About Page</h2>
+  )
+}
+function App() {
+  return (
+<BrowserRouter>
+<nav>
+  <NavLink to="/">Home</NavLink>
+  <NavLink to="/about">About</NavLink>
+  <NavLink to="/contact">Contact</NavLink>
+  <NavLink to="/productlist">ProductList</NavLink>
+</nav>
+<Routes>
+  <Route path="/" element={<Home />}></Route>
+  <Route path="/about" element={<About />}></Route>
+  <Route path="/contact" element={<Contact />}></Route>
+  <Route path="/productlist" element={<Products  list={products}/>}></Route>
+  <Route path="/productlist/:id" element={<ProductDetail list={products}/>}></Route>
+</Routes>
+</BrowserRouter>
+  
+    
+  
+)
+}
+
+export default App
---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return(
+        <> 
+        <h2>Contact Information</h2>
+        <p>Tel : 010-9816-8320</p>
+        <p>Email : me@mail.com</p>
+        </>
+    )
+}
+export default Contact;
